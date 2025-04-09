06389e44b9950e48fa215fde44f1276918093c0c
Indranil Das <indranildas06@gmail.com>
2025-04-09 07:12:18 +0530

    help menu fix

--- index.js
+++ index.js
@@ -114,26 +114,14 @@ program
 program.on("--help", () => {
   console.log("");
   console.log(chalk.magenta("Examples:"));
+  console.log(chalk.gray("  $"), "rizer image.jpg", chalk.yellow("-w 320,640"));
+  console.log(chalk.gray("  $"), "rizer image.jpg", chalk.yellow("-h 240,480"));
   console.log(
     chalk.gray("  $"),
-    "img-resizer image.jpg",
-    chalk.yellow("-w 320,640")
-  );
-  console.log(
-    chalk.gray("  $"),
-    "img-resizer image.jpg",
-    chalk.yellow("-h 240,480")
-  );
-  console.log(
-    chalk.gray("  $"),
-    "img-resizer image.jpg",
+    "rizer image.jpg",
     chalk.yellow("-r 0.25,0.5")
   );
-  console.log(
-    chalk.gray("  $"),
-    "img-resizer image.jpg",
-    chalk.yellow("-w 1920 -u")
-  );
+  console.log(chalk.gray("  $"), "rizer image.jpg", chalk.yellow("-w 1920 -u"));
   console.log(chalk.gray("  $"), "rizer image.jpg");
 });
 
